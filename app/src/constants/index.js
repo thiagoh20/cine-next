f39cbd02826de3533591338126ed14e2514498bc
thiagoh20 <thiago.bbolivar@gmail.com>
2024-11-25 11:34:45 -0500

    se quita dotenv

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://cine-nexus-api.vercel.app/?vercelToolbarCode=dfJs3NHlG8UgbQF'
 const imagenes_URL = process.env.imagenes_api_URL || 'https://image.tmdb.org/t/p/original'
 export {
