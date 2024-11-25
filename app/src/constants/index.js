57644af351ba9274bf9d209c552cc7c13cef3956
Santiago Hernandez B <87908385+thiagoh20@users.noreply.github.com>
2024-11-25 13:36:14 -0500

    Update index.js

--- app/src/constants/index.js
+++ app/src/constants/index.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://cine-nexus-api.vercel.app/?vercelToolbarCode=dfJs3NHlG8UgbQF'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://cine-nexus-api.vercel.app'
 const imagenes_URL = process.env.imagenes_api_URL || 'https://image.tmdb.org/t/p/original'
 export {
   BACKEND_URL,
